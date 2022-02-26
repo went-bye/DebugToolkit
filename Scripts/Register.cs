72232467c46f233f71c8041d45b8efbe98a54e48
went5 <went.develop@gmail.com>
2022-02-26 11:28:18 +0900

    delete visual prog

--- Scripts/Register.cs
+++ Scripts/Register.cs
@@ -8,8 +8,38 @@ using UnityEngine.UIElements;
 namespace DebugToolkit
 {
     [RequireComponent(typeof(UIDocument))]
-    public class Register : BaseRegister
+    public class Register : MonoBehaviour
     {
+        [SerializeField] protected UIDocument uiDocument;
+        protected ScrollView scrollView;
+
+        private void Reset()
+        {
+            uiDocument = GetComponent<UIDocument>();
+            uiDocument.panelSettings = Resources.Load<PanelSettings>("DebugToolkitPanelSettings");
+        }
+
+        private void OnEnable()
+        {
+            var rootVisualElement = uiDocument.rootVisualElement;
+            rootVisualElement.styleSheets.Clear();
+            var styleSheet = Resources.Load<StyleSheet>("DebugToolkitUSS");
+#if UNITY_EDITOR
+            rootVisualElement.styleSheets.Add(EditorOnlyRegister.GetDefaultSkin());
+#endif
+            rootVisualElement.styleSheets.Add(styleSheet);
+
+            // foldout
+            var foldout = new Foldout();
+            foldout.text = "DebugToolkit";
+            foldout.AddToClassList("rootFoldout");
+            rootVisualElement.Add(foldout);
+
+            // スクロールビュー
+            scrollView = new ScrollView();
+            foldout.Add(scrollView);
+        }
+
         private Dictionary<String, Logger> _text = new Dictionary<string, Logger>();
         private Dictionary<String, ProgressBar> _progress = new Dictionary<string, ProgressBar>();
 
